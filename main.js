f2ae3a0ad759c8b5e7edeee46565a061a150a719
Shenwei Wang <wangshenwei@qq.com>
2013-08-22 15:25:34 +0800

    Updates

--- main.js
+++ main.js
@@ -1,30 +1,59 @@
-
-/*global hljs, Chart, impress */
+/* global hljs, impress */
 
 hljs.tabReplace = "    ";
 hljs.initHighlighting();
 
-/*(function(){
-	var ctx = document.getElementById("canvas-github-pie").getContext("2d");
-	new Chart(ctx).Pie(data, options);
-})();*/
-
-if (! window.noImpress) {
+if (! /impress=no/.test(location.search)) {
 	var steps = document.querySelectorAll("#impress > .step"),
 		deltaX = 1200,
 		deltaY = 900,
-		x, y;
-	Array.prototype.forEach.call(steps, function(step, index){
-		if (step.id === "thanks") {
-			step.dataset.x = x * deltaX;
-			step.dataset.y = y * deltaY;
-			step.dataset.z = 1500;
-			return;
+		deltaZ = 2000,
+		right = 0,
+		down = 1,
+		under = 2,
+		upon = 3,
+		last,
+		x = 0,
+		y = 0,
+		z = 0;
+	var random = function() {
+		var next = Math.floor(Math.random() * 4);
+		if ((next === under && last === upon) || (next === upon && last === under)) {
+			return random();
 		}
-		x = index % 5;
-		y = Math.floor(index / 5);
-		step.dataset.x = x * deltaX;
-		step.dataset.y = y * deltaY;
+		switch (next) {
+			case right:
+				x += deltaX;
+				break;
+			case down:
+				y += deltaY;
+				break;
+			case under:
+				z -= deltaZ;
+				break;
+			case upon:
+				z += deltaZ;
+				break;
+			default:
+				throw "unexpected position";
+		}
+		last = next;
+	};
+	Array.prototype.forEach.call(steps, function(step, index){
+		step.dataset.x = x;
+		step.dataset.y = y;
+		step.dataset.z = z;
+		if (step.id === "about-this") {
+			if (last === under) {
+				z -= deltaZ;
+				last = under;
+			} else {
+				z += deltaZ;
+				last = upon;
+			}
+		} else {
+			random();
+		}
 	});
 	impress().init();
 }
